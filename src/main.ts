e648397ecbdbc6ee302d2ac42dcd0bdb2b42650d
Carlosmicael <carlosmlo381@gmail.com>
2025-05-29 12:23:08 -0500

    public

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,10 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+const fileInput = document.getElementById("fileInput") as HTMLInputElement;
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+fileInput.addEventListener("change", async () => {
+  const file = fileInput.files?.[0];
+  if (!file) return;
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  const text = await file.text();
+  console.log(text)
+});
+
